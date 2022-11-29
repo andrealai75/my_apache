78dc8a3d2ee3f5678ce18b59ef45431c1d94ee70
andrealai75 <lai.andrea75@gmail.com>
2022-11-29 20:14:24 +0000

    Running the test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,7 @@
 import os
 
-def package_is_installed(name):
-  os.system('dpkg -l | apache2')
+def package_installed(name):
+  return os.system('dpkg -l | grep ' + name)
 
 def test_apache_installed():
-    assert package_is_installed('apache2') == 'ss'
+    assert package_installed('vars') == 0
